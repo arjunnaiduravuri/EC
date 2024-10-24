184b8053ba034f777c03dfa13498b8ccf1afcb94
Eaglesnight <seqtoinstitutions@gmail.com>
2024-10-24 14:18:08 +0530

    change

--- src/components/fakestore.jsx
+++ src/components/fakestore.jsx
@@ -1,79 +1,76 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
-import Nav from './Nav'
-import Product from './product'
-
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Nav from './Nav';
+import Product from './Product';
 
 const Fakestore = () => {
-    const [category,setCategorys] =useState([])
-    const [product,setProducts] = useState([{}])
-    console.log(category)
-   
-    function handlecategory(){
-        fetch("https://fakestoreapi.com/products/categories")
-        .then((res)=>res.json())
-        .then((data)=>{
-         data.unshift("All")
-         setCategorys(data)
-        })
+    const [category, setCategorys] = useState([]);
+    const [product, setProducts] = useState([{}]);
+
+    const baseUrl = 'https://fakestoreapi.com';
+
+    function handleCategory() {
+        fetch(`${baseUrl}/products/categories`)
+            .then((res) => res.json())
+            .then((data) => {
+                data.unshift('All'); 
+                setCategorys(data);
+            })
+            .catch((error) => console.error('Error fetching categories:', error));
     }
-    function products(url){
+
+    function products(url) {
         fetch(url)
-        .then((responce)=>
-            responce.json()
-        )
-        .then((data)=>{
-            setProducts(data)
-            console.log(data)
-        })
-
-
+            .then((response) => response.json())
+            .then((data) => {
+                setProducts(data);
+                console.log(data); 
+            })
+            .catch((error) => console.error('Error fetching products:', error));
     }
 
-  useEffect(()=>{
-        handlecategory()
-        products("http://fakestoreapi.com/products")
-    
-    },[])
-    function HandleOption(event){
-        if (event.target.value=="All"){
-            products("http://fakestoreapi.com/products")
-        }
-        else{
-            products(`http://fakestoreapi.com/products/category/${event.target.value}`)
+    useEffect(() => {
+        handleCategory();
+        products(`${baseUrl}/products`); 
+    }, []);
 
+    function HandleOption(event) {
+        const selectedCategory = event.target.value;
+        if (selectedCategory === 'All') {
+            products(`${baseUrl}/products`); 
+        } else {
+            products(`${baseUrl}/products/category/${selectedCategory}`);
         }
-
     }
 
-  return (
-    <div>
-        <Nav/>
-        
-        <section className='mt-2 row'>
-            <aside className='col-2 ms-3'>
-              <div>
-                <label className='form-label'>select category</label>
-                <select  className='form-select' onChange={HandleOption}>
+    return (
+        <div>
+            <Nav />
+            <section className='mt-2 row'>
+                <aside className='col-2 ms-3'>
+                    <div>
+                        <label className='form-label'>Select Category</label>
+                        <select className='form-select' onChange={HandleOption}>
+                            {
+                                category.map((item) => (
+                                    <option value={item} key={item}>
+                                        {item.toUpperCase()}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </aside>
+                <main className='row col-10 d-flex flex-wrap'>
                     {
-                      category.map((item)=>
-                      <option value={item} key={item}>{item.toUpperCase()}</option>
-                    )
+                        product.map((item) => (
+                            <Product key={item.id} item={item} />
+                        ))
                     }
-                </select>
-              </div>
-            </aside>
-            <main className='row col-10 d-flex flex-wrap '>
-                {
-                    product.map((item)=>
-                         <Product item={item}/>
-                 )}
-        </main>
+                </main>
+            </section>
+        </div>
+    );
+};
 
-        </section>
-      
-    </div>
-  )
-}
-
-export default Fakestore
+export default Fakestore;
